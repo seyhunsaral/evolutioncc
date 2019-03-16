e4a711d32ad47dc952bacabe953c4e4df60d3143
Seyhun Saral <seyhunsaral@gmail.com>
2019-03-17 00:16:10 +0100

    started analysis file

--- analysis.R
+++ analysis.R
@@ -2,8 +2,21 @@ library(readr)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
-df_actions  <- read_csv("test_actions.csv")
+df_actions  <- read_csv("db_actions.csv")
+df_types  <- read_csv("db_types.csv")
+
+
 glimpse(df_actions)
 
 ggplot(df_actions,  aes(y = proportion, x = generation, color = as.factor(action))) +
-geom_line(size=0.05)
+geom_line(size=0.5)
+
+
+glimpse(df_types)
+
+ggplot(df_types,  aes(y = proportion, x = generation, color = as.factor(type))) +
+geom_line(size=0.5)
+
+df_types %>% filter(generation == 7500) %>%  select(type, proportion) %>% arrange(desc(proportion))
+
+get_type_strategy(56)
